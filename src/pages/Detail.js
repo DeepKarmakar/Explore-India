2417a9941ffb408153d7fd570017c56ae9975e54
piupaulgit <contactpiu619@gmail.com>
2022-10-20 09:45:19 +0530

    bootsrap adde

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,23 +1,40 @@
 import { React, useEffect, useState } from "react";
-import { useParams } from 'react-router-dom';
-import axios from 'axios'
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { Col, Container, Row } from "react-bootstrap";
+import { MapContainer, TileLayer } from "react-leaflet";
 
-const baseUrl = 'http://localhost:3000/';
+const baseUrl = "http://localhost:3000/";
 const Detail = () => {
-	const [mapDetails, setMapDetails] = useState(null)
-    let { id } = useParams();
-    console.log(id);
-	
-	useEffect(() => {
-		axios.get(`${baseUrl}mapData/${id}`)
-			.then(res => {
-				setMapDetails(res.data)
-			})
-	}, [])
+  const [mapDetails, setMapDetails] = useState(null);
+  let { id } = useParams();
+  console.log(id);
 
-    return(
-        <>{mapDetails && mapDetails.location}</>
-    )
-}
+  useEffect(() => {
+    axios.get(`${baseUrl}mapData/${id}`).then((res) => {
+      setMapDetails(res.data);
+    });
+  }, []);
+
+  return (
+    <div className="detail">
+      <Row>
+        <Col md={7}>
+          <MapContainer
+            center={[20.593683, 78.962883]}
+            zoom={5}
+            scrollWheelZoom={true}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+              url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+            />
+          </MapContainer>
+        </Col>
+        <Col md={5}>details</Col>
+      </Row>
+    </div>
+  );
+};
 
 export default Detail;
